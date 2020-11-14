c2e8913ec38bb9bd975d06ed4395f7fcc19044c7
markallanson <mark@allanson.org>
2020-11-14 17:28:48 +0000

    Install virtual X server for wkhtmltopdf

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,7 +1,13 @@
 yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install wkhtmltopdf
 yum install wget
+yum install xvfb
 mkdir pandoc
 wget -qO- https://github.com/jgm/pandoc/releases/download/2.11.1.1/pandoc-2.11.1.1-linux-amd64.tar.gz | tar xvzf - --strip-components 1 -C ./pandoc
 export PATH="./pandoc/bin:$PATH"
+
+printf '#!/bin/bash\nxvfb-run -a --server-args="-screen 0, 1024x768x24" /usr/bin/wkhtmltopdf -q $*' > /usr/bin/wkhtmltopdf.sh
+chmod a+x /usr/bin/wkhtmltopdf.sh
+ln -s /usr/bin/wkhtmltopdf.sh /usr/local/bin/wkhtmltopdf
+
 make all
