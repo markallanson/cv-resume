2ef8cf0cce4078b7a1421c5bdea489c704259cc4
markallanson <mark@allanson.org>
2020-11-14 17:36:52 +0000

    use correct xvfb package name from yum repo

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,7 +1,7 @@
 yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install wkhtmltopdf
 yum install wget
-yum install xvfb
+yum install xorg-x11-server-Xvfb
 mkdir pandoc
 wget -qO- https://github.com/jgm/pandoc/releases/download/2.11.1.1/pandoc-2.11.1.1-linux-amd64.tar.gz | tar xvzf - --strip-components 1 -C ./pandoc
 export PATH="./pandoc/bin:$PATH"
